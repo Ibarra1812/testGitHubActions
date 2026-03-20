51c6db24ca56d073aa74b426e0ebb3c898e5493c
Ibarra1812 <ibarraalvaro852@gmail.com>
2026-03-20 17:41:22 -0300

    set test lint

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    const message = 'Hello world';
+    res.send(message);
 });
 
 module.exports = app;
